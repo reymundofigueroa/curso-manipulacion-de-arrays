b926ae79576f81375eb9a46c682b02bfa21ea969
Reymundo Figueroa <reymundo_124@hotmail.com>
2024-07-24 21:47:56 -0600

    clase 1 y primer reto completados

---
+++ server/01-for-each.js
@@ -0,0 +1,10 @@
+const letters = ['a', 'b', 'c'];
+// usando for normal
+for (let index = 0; index < letters.length; index++){
+  const element = letters[index]
+  console.log(element)
+}
+  
+
+// usando forEach()
+letters.forEach(item => console.log(item))
