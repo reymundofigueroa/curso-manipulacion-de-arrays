b6bdcacf3fcaab20c21c1db49cc39c4e93dfc14b
Reymundo Figueroa <reymundo_124@hotmail.com>
2024-07-29 20:37:20 -0600

    clase 2 de map() completada

---
+++ server/03-map-reloaded.js
@@ -0,0 +1,37 @@
+const orders = [
+  { customerName: "Nicolas", total: 60, delivered: true },
+  { customerName: "Zulema", total: 120, delivered: false },
+  { customerName: "Santiago", total: 180, delivered: true },
+  { customerName: "Valentina", total: 240, delivered: true },
+];
+
+console.log('original', orders)
+
+const rta = orders.map(item => item.total)
+
+console.log('rta', rta)
+
+
+// intentamos añadir un nuevo valor a cada objeto pero sin modificar el original
+// lo cual no funciona porque estamos copiando referencia a memoria y por tanto mutamos el array
+// esto no applica con elementos primitivos
+
+/*
+const rta2 = orders.map(item => {
+  item.tax = .19
+  return tax
+})
+
+console.log('rta2', rta2)
+console.log('original', orders)
+*/
+// aqui una solucion correcta usando el spread operator 
+
+const rta3 = orders.map(item => {
+  return {
+    ...item,
+    tax: 0.19
+  }
+})
+console.log('rta3', rta3)
+console.log('original', orders)
